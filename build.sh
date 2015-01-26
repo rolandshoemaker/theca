3e92a7b033c9de72cb3bcb5dfc456c4755d6ae46
Roland Shoemaker <rolandshoemaker@gmail.com>
2015-01-26 15:22:41 -0800

    test harness tabs and build script stuff

--- build.sh
+++ build.sh
@@ -15,72 +15,77 @@ INSTALL_DIR="/usr/bin"
 MAN_DIR="/usr/local/share/man/man1"
 
 case "$1" in
-	build)
-		if command -v cargo >/dev/null 2>&1; then
-			BUILD_CMD="cargo build"
-			shift
-			for arg in "$@"; do
-				BUILD_CMD="$BUILD_CMD $arg"
-			done
-			eval "$BUILD_CMD"
-			if [ "$?" -eq "0" ]; then
-				if [[ $@ =~ "--release" ]]; then
-					echo $"built target/release/theca"
-					echo $"copying target/release/theca to ."
-					cp target/release/theca .
-				else
-					echo $"built target/theca"
-					echo $"copying target/theca to ."
-					cp target/theca .
-				fi
-			else
-				echo $"couldn't build target/theca"
-			fi
-		else
-			echo "cargo could not be found"
-			exit 1
-			# there is probably a hardway to do this with
-			# just rustc... but w/e for now
-		fi
-		;;
+    build)
+        if command -v cargo >/dev/null 2>&1; then
+            BUILD_CMD="cargo build"
+            shift
+            for arg in "$@"; do
+                BUILD_CMD="$BUILD_CMD $arg"
+            done
+            cargo update
+            echo $"# dependencies udated"
+            eval "$BUILD_CMD"
+            if [ "$?" -eq "0" ]; then
+                if [[ $@ =~ "--release" ]]; then
+                    echo $"# built target/release/theca"
+                    cp target/release/theca .
+                    echo $"# copied target/release/theca to ."
+                else
+                    echo $"# built target/theca"
+                    echo $"# copied target/theca to ."
+                    cp target/theca .
+                fi
+            else
+                echo $"couldn't build target/theca"
+            fi
+        else
+            echo $"cargo could not be found"
+            exit 1
+            # there is probably a hard way to do this with
+            # just rustc... but w/e for now
+        fi
+        ;;
 
-	build-man)
-		if command -v md2man-roff >/dev/null 2>&1; then
-			md2man-roff docs/THECA.1.md > docs/THECA.1
-			echo $"built THECA.1 man page"
-		else
-			echo $"md2man-roff could not be found"
-			exit 1
-		fi
-		;;
+    build-man)
+        if command -v md2man-roff >/dev/null 2>&1; then
+            md2man-roff docs/THECA.1.md > docs/THECA.1
+            echo $"# built THECA.1 man page"
+        else
+            echo $"# md2man-roff could not be found"
+            exit 1
+        fi
+        ;;
 
-	install)
-		if [ -e "theca" ]; then
-			echo $"copying ./theca -> $INSTALL_DIR/theca"
-			cp theca $INSTALL_DIR/
-		fi
-		if [[ $@ =~  "--man" ]]; then
-			if [ -e "docs/THECA.1" ]; then
-				echo $"copying docs/THECA.1 -> $MAN_DIR/THECA.1"
-				cp docs/THECA.1 $MAN_DIR/
-			fi
-		fi
-		;;
+    install)
+        if [ -e "theca" ]; then
+            cp theca $INSTALL_DIR/
+            echo $"# copied ./theca -> $INSTALL_DIR/theca"
+        fi
+        if [[ $@ =~  "--man" ]]; then
+            if [ -e "docs/THECA.1" ]; then
+                cp docs/THECA.1 $MAN_DIR/
+                echo $"# copied docs/THECA.1 -> $MAN_DIR/THECA.1"
+            fi
+        fi
+        ;;
 
-	clean)
-		if [ -d "target" ]; then
-			rm -r target
-		fi
-		if [ -e "theca" ]; then
-			rm theca
-		fi
-		if [ -e "docs/THECA.1" ]; then
-			rm docs/THECA.1
-		fi
-		;;
+    clean)
+        if [ -d "target" ]; then
+            rm -r target
+            echo $"# deleted ./target/"
+        fi
+        if [ -e "theca" ]; then
+            rm theca
+            echo $"# deleted ./theca"
+        fi
+        if [ -e "docs/THECA.1" ]; then
+            rm docs/THECA.1
+            echo $"# deleted ./docs/THECA.1"
+        fi
+        ;;
 
-	*)
-		echo $"Usage: $0 {build|build-man|install|clean}"
-		exit 1
-
+    *)
+        echo $"Usage: $0 {build|build-man|install|clean}"
+        exit 1
+        ;;
 esac
--- tests/theca_test_harness.py
+++ tests/theca_test_harness.py
@@ -23,121 +23,121 @@ import os
 import time
 
 def decrypt_profile(ciphertext, passphrase):
-  key = pbkdf2(bytes(passphrase.encode("utf-8")), sha256(bytes(passphrase.encode("utf-8"))).hexdigest().encode("utf-8"), 2056, 32, "hmac-sha256")
-  iv = ciphertext[0:16]
-  decryptor = AES.new(key, AES.MODE_CBC, iv)
-  plaintext = decryptor.decrypt(ciphertext[16:])
-  try:
-    return plaintext[:-plaintext[-1]].decode("utf-8")
-  except UnicodeDecodeError:
-    raise AssertionError("profile could not be decrypted")
+    key = pbkdf2(bytes(passphrase.encode("utf-8")), sha256(bytes(passphrase.encode("utf-8"))).hexdigest().encode("utf-8"), 2056, 32, "hmac-sha256")
+    iv = ciphertext[0:16]
+    decryptor = AES.new(key, AES.MODE_CBC, iv)
+    plaintext = decryptor.decrypt(ciphertext[16:])
+    try:
+        return plaintext[:-plaintext[-1]].decode("utf-8")
+    except UnicodeDecodeError:
+        raise AssertionError("profile could not be decrypted")
 
 def read_enc_json_file(path, pp):
-  with open(path, "rb") as f:
-    data = f.read()
-  try:
-    return json.loads(decrypt_profile(data, pp))
-  except ValueError:
-    raise AssertionError("profile contains invalid json")
+    with open(path, "rb") as f:
+        data = f.read()
+    try:
+        return json.loads(decrypt_profile(data, pp))
+    except ValueError:
+        raise AssertionError("profile contains invalid json")
 
 def read_json_file(path):
-  a = open(path)
-  try:
-    return json.load(a)
-  except ValueError:
-    raise AssertionError("profile contains invalid json")
+    a = open(path)
+    try:
+        return json.load(a)
+    except ValueError:
+        raise AssertionError("profile contains invalid json")
 
 def validate_profile_schema(profile, schema):
-  validate_schema(profile, schema)
+    validate_schema(profile, schema)
 
 def validate_profile_contents(profile):
-  for i, n in enumerate(profile['notes']):
-    if i > 0 and i < len(profile['notes'])-1:
-      if not profile['notes'][i-1]['id'] < n['id'] < profile['notes'][i+1]['id']:
-        raise AssertionError("object #"+str(i)+" id is out of order ("+str(profile['notes'][i-1]['id'])+", "+str(n['id'])+", "+str(profile['notes'][i+1]['id'])+")")
-      if n['status'] not in STATUSES: raise AssertionError("")
-    if n['id'] < 0: raise AssertionError("object #"+str(i)+" id is negative ("+str(n['id'])+")")
-    try:
-      strptime(n['last_touched'], DATEFMT)
-    except ValueError:
-      print(n)
-      raise AssertionError("object #"+str(i)+" last_touched doesn't match time format "+DATEFMT)
-    profile_ids = [n['id'] for n in profile['notes']]
-    if len(profile_ids) != len(set(profile_ids)): raise AssertionError("there are duplicate IDs in 'notes'")
+    for i, n in enumerate(profile['notes']):
+        if i > 0 and i < len(profile['notes'])-1:
+            if not profile['notes'][i-1]['id'] < n['id'] < profile['notes'][i+1]['id']:
+                raise AssertionError("object #"+str(i)+" id is out of order ("+str(profile['notes'][i-1]['id'])+", "+str(n['id'])+", "+str(profile['notes'][i+1]['id'])+")")
+            if n['status'] not in STATUSES: raise AssertionError("")
+        if n['id'] < 0: raise AssertionError("object #"+str(i)+" id is negative ("+str(n['id'])+")")
+        try:
+            strptime(n['last_touched'], DATEFMT)
+        except ValueError:
+            print(n)
+            raise AssertionError("object #"+str(i)+" last_touched doesn't match time format "+DATEFMT)
+        profile_ids = [n['id'] for n in profile['notes']]
+        if len(profile_ids) != len(set(profile_ids)): raise AssertionError("there are duplicate IDs in 'notes'")
 
 def compare_profile(clean, dirty):
-  if not clean['encrypted'] == dirty['encrypted']: raise AssertionError()
-  if not len(clean['notes']) == len(dirty['notes']): raise AssertionError()
-  for c, d in zip(clean['notes'], dirty['notes']):
-    if not c['id'] == d['id']: raise AssertionError()
-    if not c['title'] == d['title']: raise AssertionError()
-    if not c['status'] == d['status']: raise AssertionError()
-    if not c['body'] == d['body']: raise AssertionError()
-    # uh leaving last_touched for now...
+    if not clean['encrypted'] == dirty['encrypted']: raise AssertionError()
+    if not len(clean['notes']) == len(dirty['notes']): raise AssertionError()
+    for c, d in zip(clean['notes'], dirty['notes']):
+        if not c['id'] == d['id']: raise AssertionError()
+        if not c['title'] == d['title']: raise AssertionError()
+        if not c['status'] == d['status']: raise AssertionError()
+        if not c['body'] == d['body']: raise AssertionError()
+        # uh leaving last_touched for now...
 
 def test_harness(tests):
-  TMPDIR = tempfile.mkdtemp()
-  devnull = open(os.devnull, "w")
-  failed = 0
-  SCHEMA = read_json_file(SCHEMA_PATH)
+    TMPDIR = tempfile.mkdtemp()
+    devnull = open(os.devnull, "w")
+    failed = 0
+    SCHEMA = read_json_file(SCHEMA_PATH)
 
-  print("# {}\n#    {}".format(tests['title'], tests['desc']))
-  print("#\n# running {} tests.\n".format(len(tests['tests'])))
-  for t in tests['tests']:
-    try:
-      cmd = [THECA_CMD]
-      if not t["profile"] == "":
-        cmd += ["-p", t["profile"]]
-      if not t["profile_folder"] == "":
-        cmd += ["-f", os.path.join(TMPDIR, t["profile_folder"])]
-      else:
-        cmd += ["-f", TMPDIR]
-      
-      if len(t["stdin"]) > 0:
-        for c, s in zip(t["cmds"], t["stdin"]):
-          if not s == None:
-            p = subprocess.Popen(cmd+c, stdin=subprocess.PIPE, stdout=devnull)
-            p.communicate(input=bytes(s.encode('utf-8')))
-          else:
-            subprocess.call(cmd+c, stdout=devnull)
-      else:
-        for c in t["cmds"]:
-          subprocess.call(cmd+c, stdout=devnull)
+    print("# {}\n#    {}".format(tests['title'], tests['desc']))
+    print("#\n# running {} tests.\n".format(len(tests['tests'])))
+    for t in tests['tests']:
+        try:
+            cmd = [THECA_CMD]
+            if not t["profile"] == "":
+                cmd += ["-p", t["profile"]]
+            if not t["profile_folder"] == "":
+                cmd += ["-f", os.path.join(TMPDIR, t["profile_folder"])]
+            else:
+                cmd += ["-f", TMPDIR]
+            
+            if len(t["stdin"]) > 0:
+                for c, s in zip(t["cmds"], t["stdin"]):
+                    if not s == None:
+                        p = subprocess.Popen(cmd+c, stdin=subprocess.PIPE, stdout=devnull)
+                        p.communicate(input=bytes(s.encode('utf-8')))
+                    else:
+                        subprocess.call(cmd+c, stdout=devnull)
+            else:
+                for c in t["cmds"]:
+                    subprocess.call(cmd+c, stdout=devnull)
 
-      result_path = os.path.join(TMPDIR, t["result_path"])
-      if t["result"]["encrypted"]:
-        json_result = read_enc_json_file(result_path, t["result_passphrase"])
-      else:
-        json_result = read_json_file(result_path)
-      validate_profile_schema(json_result, SCHEMA)
-      validate_profile_contents(json_result)
-      compare_profile(t["result"], json_result)
-      print("\ttest: "+t['name']+" [PASSED]")
-    except (AssertionError, FileNotFoundError) as e:
-      print("\033[91m"+"\ttest: "+t['name']+" [FAILED]"+"\033[0m")
-      failed += 1
+            result_path = os.path.join(TMPDIR, t["result_path"])
+            if t["result"]["encrypted"]:
+                json_result = read_enc_json_file(result_path, t["result_passphrase"])
+            else:
+                json_result = read_json_file(result_path)
+            validate_profile_schema(json_result, SCHEMA)
+            validate_profile_contents(json_result)
+            compare_profile(t["result"], json_result)
+            print("\ttest: "+t['name']+" [PASSED]")
+        except (AssertionError, FileNotFoundError) as e:
+            print("\033[91m"+"\ttest: "+t['name']+" [FAILED]"+"\033[0m")
+            failed += 1
 
-    # os.remove(result_path)
-    for f_o in os.listdir(TMPDIR):
-      f_o_p = os.path.join(TMPDIR, f_o)
-      if os.path.isfile(f_o_p):
-        os.unlink(f_o_p)
-      else:
-        shutil.rmtree(f_o_p)
+        # os.remove(result_path)
+        for f_o in os.listdir(TMPDIR):
+            f_o_p = os.path.join(TMPDIR, f_o)
+            if os.path.isfile(f_o_p):
+                os.unlink(f_o_p)
+            else:
+                shutil.rmtree(f_o_p)
 
-  rmtree(TMPDIR)
-  devnull.close()
-  print("\n[passed: {}, failed {}]\n".format(len(tests['tests'])-failed, failed))
-  return failed
+    rmtree(TMPDIR)
+    devnull.close()
+    print("\n[passed: {}, failed {}]\n".format(len(tests['tests'])-failed, failed))
+    return failed
 
 # GOOD_TESTS = read_json_file("tests/good_tests.json")
 # BAD_TESTS = read_json_file("tests/bad_tests.json")
 
 ALL_TESTS = [
-  "tests/good_default_tests.json",
-  "tests/good_second_profile_tests.json",
-  "tests/good_encrypted_profile_tests.json",
-  "tests/bad_tests.json"
+    "tests/good_default_tests.json",
+    "tests/good_second_profile_tests.json",
+    "tests/good_encrypted_profile_tests.json",
+    "tests/bad_tests.json"
 ]
 
 THECA_CMD = "theca"
@@ -147,19 +147,19 @@ DATEFMT = "%Y-%m-%d %H:%M:%S %z"
 SCHEMA_PATH = "schema.json"
 
 if __name__ == "__main__":
-  test_sum = 0
-  failed = 0
-  start = time.time()
-  for t_set_path in ALL_TESTS:
-    t_set = read_json_file(t_set_path)
-    test_sum += len(t_set['tests'])
-    failed += test_harness(t_set)
-  elapsed = time.time()-start
-  m, s = divmod(elapsed, 60)
-  h, m = divmod(m, 60)
+    test_sum = 0
+    failed = 0
+    start = time.time()
+    for t_set_path in ALL_TESTS:
+        t_set = read_json_file(t_set_path)
+        test_sum += len(t_set['tests'])
+        failed += test_harness(t_set)
+    elapsed = time.time()-start
+    m, s = divmod(elapsed, 60)
+    h, m = divmod(m, 60)
 
-  print("ran %d tests overall: %d passed, %d failed, took %02d:%02d:%02d\n" % (test_sum, test_sum-failed, failed, h, m, s))
+    print("ran %d tests overall: %d passed, %d failed, took %02d:%02d:%02d\n" % (test_sum, test_sum-failed, failed, h, m, s))
 
-  if failed > 0:
-    print("\033[91m"+"BAD"+"\033[0m")
-    exit(1)
+    if failed > 0:
+        print("\033[91m"+"BAD"+"\033[0m")
+        exit(1)
