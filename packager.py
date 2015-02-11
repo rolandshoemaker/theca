6929ec2492118b794fdd4683f96c233bbd12fdb6
Roland Shoemaker <rolandshoemaker@gmail.com>
2015-02-11 03:26:40 -0800

    better-ish packager, time to sleep

--- packager.py
+++ packager.py
@@ -1,10 +1,10 @@
-import platform, os, shutil, tempfile, tarfile
+import platform, os, shutil, tempfile, tarfile, argparse
 from subprocess import Popen, PIPE
 
 # *Should* be run on clean git clone from the root of the repo...
 
-def s_Popen(cmd, return_output=False):
-	p = Popen(cmd, stdout=PIPE, stderr=PIPE)
+def s_Popen(cmd, return_output=False, **kwargs):
+	p = Popen(cmd, stdout=PIPE, stderr=PIPE, **kwargs)
 	output = p.communicate()[0].decode("utf-8")
 	if p.returncode > 0:
 		# raise or something idk?
@@ -12,10 +12,21 @@ def s_Popen(cmd, return_output=False):
 	if return_output:
 		return output
 
-OUTPUT_DIR = "/home/roland/testing"
+parser = argparse.ArgumentParser(description="")
+parser.add_argument("PREFIX", help="package prefix")
+parser.add_argument("COMMIT_HASH", help="commit to build from")
+parser.add_argument("OUTPUT_DIR", help="directory to place packaged tarballs")
+parser.add_argument("--rust-channel", default="nightly", help="rust release channel to use")
+parser.add_argument("--target_arch")
+parser.add_argument("--clone-depth", default="50")
+args = parser.parse_args()
 
-RUST_CHANNEL = "nightly"
-TARGET_ARCHS = ["x86_64", "i686"]
+GIT_REPO = "https://github.com/rolandshoemaker/theca"
+
+OUTPUT_DIR = args.OUTPUT_DIR
+
+RUST_CHANNEL = args.rust_channel
+TARGET_ARCHS = args.target_arch or ["x86_64", "i686"]
 if platform.system() == "Linux":
 	HOST_OS = "unknown-linux-gnu"
 elif platform.system() == "Darwin":
@@ -24,7 +35,7 @@ else:
     print("nop")
     exit(1)
 
-PACKAGE_PREFIX = "theca-nightly" # from arg!!
+PACKAGE_PREFIX = args.PREFIX
 PACKAGE_STATIC_CONTENT = {
 	"install.sh": "install.sh",
 	"README.md": "README.md",
@@ -39,13 +50,19 @@ BUILD_CMD = ["cargo", "build", "--release"]
 # setup tmpdir
 TMPDIR = tempfile.mkdtemp()
 
+# clone git repo
+s_Popen(["git", "clone", "--depth="+args.clone_depth, GIT_REPO], cwd=TMPDIR) # --branch too?
+# checkout right commit
+git_folder = os.path.join(TMPDIR, GIT_REPO.split("/")[-1])
+s_Popen(["git", "checkout", "-qf", args.COMMIT_HASH], cwd=git_folder)
+
 # build static package structure 
 package_dir = os.path.join(TMPDIR, PACKAGE_PREFIX)
 os.makedirs(package_dir)
 
 for from_path, to_path in PACKAGE_STATIC_CONTENT.items():
 	# relativize pathes
-	rel_from_path = os.path.join(os.getcwd(), from_path)
+	rel_from_path = os.path.join(git_folder, from_path)
 	rel_to_path = os.path.join(package_dir, to_path)
 	# if folder tree doesnt exist make it
 	if not os.path.exists(os.path.split(rel_to_path)[0]):
@@ -66,14 +83,19 @@ start_toolchain = s_Popen(["multirust", "show-default"], return_output=True).spl
 
 # build package in package_dir for arch + output package in OUTPUT_DIR
 for t_a in TARGET_ARCHS:
-	cargo_release_target = os.path.join(os.getcwd(), "target", "release", "theca")
+	cargo_release_target = os.path.join(git_folder, "target", "release", "theca")
 	package_binary_path = os.path.join(binary_folder, "theca")
-	# set multirust to the right toolchain
+
+	# make sure toolchain is up to date
 	current_toolchain = "-".join([RUST_CHANNEL, t_a, HOST_OS])
+	toolchain_installer_url = "https://static.rust-lang.org/dist/rust-%s.tar.gz" % (current_toolchain)
+	s_Popen(["multirust", "update", current_toolchain, "--installer", toolchain_installer_url])
+
+	# set the right toolchain
 	s_Popen(["multirust", "default", current_toolchain])
 
 	# build package
-	build_output = s_Popen(BUILD_CMD, return_output=True)
+	build_output = s_Popen(BUILD_CMD, return_output=True, cwd=git_folder)
 
 	# copy binary to binary_folder
 	shutil.copy2(cargo_release_target, package_binary_path)
