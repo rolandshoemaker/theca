4fbe030eb6c1f3b78cf28da605d6ad79fa353289
Roland Shoemaker <rolandshoemaker@gmail.com>
2015-02-12 01:08:24 -0800

    refactored install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -11,15 +11,30 @@
 #   simple bash script to install binaries, man page, bash+zsh completion
 #   etc and run the first time stuff for the binary packages
 
-INSTALL_PREFIX="/usr"
+INSTALL_PREFIX="/usr/local"
 FOLDERS_TO_INSTALL="bin etc share"
 
-echo $"#  _   _                    "
-echo $"# | |_| |__   ___  ___ __ _ "
-echo $"# | __| '_ \ / _ \/ __/ _\` |"
-echo $"# | |_| | | |  __/ (_| (_| |"
-echo $"#  \__|_| |_|\___|\___\__,_|"
-echo $"#"
+p() {
+	echo $"theca-installer: $1"
+}
+
+err() {
+	p "ERROR: $1" >&2
+	exit 1
+}
+
+ok() {
+	if [ $? != 0 ]; then
+		err "$1"
+	fi
+}
+
+p "#  _   _                    "
+p "# | |_| |__   ___  ___ __ _ "
+p "# | __| '_ \ / _ \/ __/ _\` |"
+p "# | |_| | | |  __/ (_| (_| |"
+p "#  \__|_| |_|\___|\___\__,_|"
+p "#"
 
 if ! [[ -w "$INSTALL_PREFIX" ]]; then
 	# if you don't have priv to write to INSTALL_PREFIX invoke 'sudo' before 'cp'
@@ -29,58 +44,49 @@ fi
 # copy all the stuff in FOLDERS_TO_INSTALL to INSTALL_PREFIX with parent directories
 # yuh yuh
 if [[ -e $INSTALL_PREFIX ]]; then
-	echo $"# installing theca"
+	p "# installing theca"
 	for f in `find $FOLDERS_TO_INSTALL`; do
 		if ! [[ -d "$f" ]]; then
 			$PRIV_ESC cp --parents $f $INSTALL_PREFIX
-			if [ "$?" -eq "0" ]; then
-				echo $"# copied $f -> $INSTALL_PREFIX/$f"
-			else
-				echo $"# ERROR: couldn't copy $f -> $INSTALL_PREFIX/$f"
-				exit 1
-			fi
+			ok "couldn't copy $f -> $INSTALL_PREFIX/$f"
+			p "# copied $f -> $INSTALL_PREFIX/$f"
 		fi
 	done
 else
-	echo $"# ERROR: $INSTALL_PREFIX doesn't exist"
-	exit 1
+	err "$INSTALL_PREFIX doesn't exist"
 fi
 
-echo $"#"
-echo $"# installed `theca --version`"
-echo $"#"
+if ! command -v theca > /dev/null 2>&1; then
+	err "can't run `theca` after install, not sure what's up with that"
+fi
+
+p "#"
+p "# installed `theca --version`"
+p "#"
 
 # first run type stuff
-echo $"# would you like to setup the default profile folder and profile for theca?"
-echo $"# this will create:"
-echo $"#   $HOME/.theca"
-echo $"#   $HOME/.theca/default.json"
-echo $"# which can also be done with 'theca new-profile'"
+p "# would you like to setup the default profile folder and profile for theca?"
+p "# this will create:"
+p "#   $HOME/.theca"
+p "#   $HOME/.theca/default.json"
+p "# which can also be done with 'theca new-profile'"
 select yn in "Yes" "No"; do
 	case $yn in
 		Yes)
-			echo $"#"
+			p "#"
 			mkdir $HOME/.theca
-			if [ "$?" -eq "0" ]; then
-				echo $"# created $HOME/.theca"
-			else
-				echo $"# ERROR: couldn't create $HOME/.theca"
-				exit 1
-			fi
+			ok "couldn't create $HOME/.theca"
+			p "# created $HOME/.theca"
 			theca new-profile
-			if [ "$?" -eq "0" ]; then
-				echo $"# created the default profile"
-				echo $"#"
-				echo $"# HAVE A FUN TIME"
-			else
-				echo $"# ERROR: couldn't create the default profile"
-				exit 1
-			fi
+			ok "couldn't create default profile, this seems bad..."
+			p "# created the default profile"
+			p "#"
+			p "# HAVE A FUN TIME"
 			break
 		;;
 		No)
-			echo $"#"
-			echo $"# ok bye!"
+			p "#"
+			p "# ok bye!"
 			exit
 		;;
 	esac
